fbdf8db67faecbc59d02fbb2b818619a1f41778c
michaelboyo <103725264+MichaelBoyo@users.noreply.github.com>
2023-07-25 16:06:57 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import cron from 'node-cron';
 
 cron.schedule(`*/1 * * * *`, async () => {
   console.log(`running your task...`);
-  fetch("https://foreign-exchange-api-dev.up.railway.app/api/exchange/v1/currency/all").then(res => console.log(res)).catch(err=> console.log(err))
+  fetch(process.env.CUSTOM_URL).then(res => console.log(res)).catch(err=> console.log(err))
 });

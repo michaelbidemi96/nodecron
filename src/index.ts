5001541b85bbe354da1ef25ac66c18db655e2c89
michaelboyo <103725264+MichaelBoyo@users.noreply.github.com>
2023-07-25 16:08:11 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import cron from 'node-cron';
 
 cron.schedule(`*/1 * * * *`, async () => {
   console.log(`running your task...`);
-  fetch(process.env.CUSTOM_URL).then(res => console.log(res)).catch(err=> console.log(err))
+  fetch(process.env.CUSTOM_URL || "").then(res => console.log(res)).catch(err=> console.log(err))
 });

f485f5942bbbd7f8b1d713810e28bc08885836c5
michaelboyo <103725264+MichaelBoyo@users.noreply.github.com>
2023-07-25 16:02:31 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import cron from 'node-cron';
 
 cron.schedule(`*/1 * * * *`, async () => {
   console.log(`running your task...`);
+  fetch("https://foreign-exchange-api-dev.onrender.com/swagger-ui/index.html#/kyc-controller/handleFileUpload").then(res => console.log(res)).catch(err=> console.log(err))
 });

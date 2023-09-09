22ea8357c4951f16c7fe8c4cdbaa9e9d4b2f6f6e
michaelbidemi96 <143907528+michaelbidemi96@users.noreply.github.com>
2023-09-09 15:12:33 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ cron.schedule(`*/1 * * * *`, async () => {
   console.log(`running your task... `);
   fetch(process.env.CUSTOM_URL || "").then(res => console.log(res)).catch(err=> console.log(err))
   fetch(process.env.CUSTOM_URL_2 || "").then(res => console.log(res)).catch(err=> console.log(err))
+   fetch(process.env.CUSTOM_URL_3 || "").then(res => console.log(res)).catch(err=> console.log(err))
 });
